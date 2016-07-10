a5e7e2a55278deb6679a0356aa42581a8b099fec
toxazol <tritoncheg@gmail.com>
2016-07-10 14:44:36 +0300

    Добавил комментарии к модулю

--- src/obfuscator.js
+++ src/obfuscator.js
@@ -1,19 +1,25 @@
 /**
- * Модуль минифицирует и обфусцирует CSS классы
- * @param {Array} data – массив CSS классов
- * @return {Object} obj
+ * Модуль минифицирует и обфусцирует CSS классы.
+ * @param {Array} data – массив CSS классов.
+ * @return {Object} obj - объект с ключами в виде старых.
+ * CSS классов и значениями в виде новых.
  */
 module.exports = function(data){
 	/**
-	 * Description
+	 * Вспомогательный метод для работы со строками.
 	 * @method replaceAt
-	 * @param {} index
-	 * @param {} character
-	 * @return BinaryExpression
+	 * @param {number} index
+	 * @param {String} character
+	 * @return {String}
 	 */
 	String.prototype.replaceAt=function(index, character) {
 	    return this.substr(0, index) + character + this.substr(index+character.length);
 	}
+	/**
+	 * Массив, хранящий допустимые символы
+	 * для генерации новых имен классов.
+	 * @type {Array}
+	 */
 	var alphaNum = [];
 	var i = 'a'.charCodeAt(0), z = 'z'.charCodeAt(0);
 	for(;i <= z; i++)
@@ -25,50 +31,73 @@ module.exports = function(data){
 	var l2 = alphaNum.length-1;
 
 	/**
-	 * Description
-	 * @method nextName
-	 * @param {} name
-	 * @return 
+	 * Возвращает следующее имя класса.
+	 * Принцип действия наподобие инкрементирования
+	 * в позиционной системе счисления.
+	 * @function nextName
+	 * @param {String} name - предыдшествующее сгенерированное
+	 * имя класса.
+	 * @return {String} - следующее имя.
 	 */
 	function nextName(name){
 		for(var i=name.length-1;i>0;i--){
+			// начиная с последней буквы имени
 			var num = alphaNum.indexOf(name[i]);
+			// получить номер в алфавите
 			if(num < l2){
+				// если возможно заменить символ на следующий
 				name = name.replaceAt(i, alphaNum[num+1]);
 				return name;
 			}
 			else{
+				// иначе на первый символ
 				name = name.replaceAt(i, 'a');
 			}
 		}
 		var num = alphaNum.indexOf(name[0]);
+		// первая буква
 		if(num < l1){
+			// если возможно заменить символ на следующий
 			name = name.replaceAt(0, alphaNum[num+1]);
 			return name;
 		}
 		else{
+			// иначе на первый символ
+			// и добавать еще один разряд
 			name = name.replaceAt(0, 'a');
 			return 'a' + name;
 		}
 			
 	};
+	/**
+	 * Инициализируем объект.
+	 * ключи - старые имена классов.
+	 * значения - количество совпадений.
+	 * @type {Object}
+	 */
 	var obj = {};
 	for(i of data){
 		if(!obj[i])obj[i] = 1;
 		else obj[i]++;
 	}
-
+	/**
+	 * Массив для сортировки по частоте
+	 * и генерации новых имен.
+	 * @type {Array}
+	 */
 	var sortable = [];
 	for (i in obj)
 		sortable.push([i, obj[i]]);
 	sortable.sort(function(a,b){
 		return b[1]-a[1];
 	});
-
+	// генерируем имена начиная с самого часто
+	// встречающегося имени класса
 	var prev = sortable[0][1] = 'a';
 	for(i=1;i<sortable.length;i++)
 		prev = sortable[i][1] = nextName(prev);
 
+	// возращаем значения в объект
 	obj = {};
 	for(i of sortable)
 		obj[i[0]] = i[1];
